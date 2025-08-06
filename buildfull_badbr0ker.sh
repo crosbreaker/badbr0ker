32531b18987a5bf0705695c4264b775a581782ee
HarryJarry <148587892+HarryJarry1@users.noreply.github.com>
2025-08-06 17:03:37 +0000

    Update buildfull_badbr0ker.sh

--- buildfull_badbr0ker.sh
+++ buildfull_badbr0ker.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-# simple paththrough script
+# simple passthrough script
+
 board=$1
 recopath=$2
-fail(){
-	printf "$1\n"
-	printf "error occurred\n"
-	exit
+
+fail() {
+    printf "%s\n" "$1"
+    printf "error occurred\n"
+    exit 1
 }
+
+if [ -z "$board" ] || [ -z "$recopath" ]; then
+    fail "Usage: $0 <board> <recovery_path>"
+fi
+
 echo "running update_downloader.sh"
-bash update_downloader.sh "$board" || fail "update_downloader.sh exited with an erorr"
+bash update_downloader.sh "$board" || fail "update_downloader.sh exited with an error"
+
 echo "running build_badrecovery.sh"
 ./build_badrecovery.sh -i "$recopath" -t unverified || fail "build_badrecovery.sh exited with an error"
-echo "
+
+echo "No errors detected while buildng the badbr0ker image"
