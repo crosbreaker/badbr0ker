2a044f9ad9ab8749bb170567800e5ecb25a9a87c
HarryJarry <148587892+HarryJarry1@users.noreply.github.com>
2025-08-30 17:40:51 -0400

    hopefully fix eve

--- buildfull_badbr0ker.sh
+++ buildfull_badbr0ker.sh
@@ -7,10 +7,15 @@ fail() {
     printf "error occurred\n" >&2
     exit 1
 }
+if [ "$board" = "eve" ]; then
+    recoveryver=126
+else
+    recoveryver=129
+fi
 findimage(){ # Taken from murkmod
     echo "Attempting to find recovery image from https://github.com/MercuryWorkshop/chromeos-releases-data data..."
     local mercury_data_url="https://raw.githubusercontent.com/MercuryWorkshop/chromeos-releases-data/refs/heads/main/data.json"
-    local mercury_url=$(curl -ks "$mercury_data_url" | jq -r --arg board "$board" --arg version 129 '
+    local mercury_url=$(curl -ks "$mercury_data_url" | jq -r --arg board "$board" --arg version "$recoveryver" '
       .[$board].images
       | map(select(
           .channel == "stable-channel" and
